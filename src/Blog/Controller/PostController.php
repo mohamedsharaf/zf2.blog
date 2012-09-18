312a59a2183e688d215e485ac51373b5388211ad
Haiden Taylor <haiden@asciant.com>
2012-09-18 19:29:58 +1000

    Following zf2 guide, struggling wioth routes. bah.

--- src/Blog/Controller/PostController.php
+++ src/Blog/Controller/PostController.php
@@ -47,4 +47,37 @@ class PostController extends AbstractActionController
         }
         return array('form' => $form);
     }
+    
+    public function editAction()
+    {
+        $id = (int) $this->params()->fromRoute('id', 0);
+        if (!$id) {
+            return $this->redirect()->toRoute('blog', array(
+                'action' => 'add'
+            ));
+        }
+        $post = $this->getPostTable()->getPost($id);
+
+        $form  = new PostForm();
+        $form->bind($post);
+        $form->get('submit')->setAttribute('value', 'Edit');
+
+        $request = $this->getRequest();
+        if ($request->isPost()) {
+            $form->setInputFilter($post->getInputFilter());
+            $form->setData($request->getPost());
+
+            if ($form->isValid()) {
+                $this->getPostTable()->savePost($post);
+
+                // Redirect to list of albums
+                return $this->redirect()->toRoute('blog');
+            }
+        }
+
+        return array(
+            'id' => $id,
+            'form' => $form,
+        );
+    }
 }
--- src/Blog/Model/Post.php
+++ src/Blog/Model/Post.php
@@ -2,7 +2,12 @@
 
 namespace Blog\Model;
 
-class Post
+use Zend\InputFilter\Factory as InputFactory;
+use Zend\InputFilter\InputFilter;
+use Zend\InputFilter\InputFilterAwareInterface;
+use Zend\InputFilter\InputFilterInterface;
+
+class Post implements InputFilterAwareInterface
 {
     public $id;
     public $title;
@@ -76,5 +81,10 @@ class Post
         }
 
         return $this->inputFilter;
+    }
+    
+    public function getArrayCopy()
+    {
+        return get_object_vars($this);
     }
 }
